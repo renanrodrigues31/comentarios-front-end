8d46af3a74390b8092bd174771bffccc6fadb25e
Renan <98278523+renanrodrigues31@users.noreply.github.com>
2022-03-11 16:12:52 -0300

    atualizando o js

--- app.js
+++ app.js
@@ -1,69 +1,59 @@
 const send = document.querySelector('.escrever-comentario button');
 const reply = document.querySelectorAll('.reply');
-const buttonDelete = document.querySelectorAll('.delete');
+const buttonDelete = document.querySelector('.delete');
 const edit = document.querySelectorAll('.edit');
 const likeAdicionar = document.querySelectorAll('.icon-plus');
 const likeSubtrair = document.querySelectorAll('.icon-minus');
+const idDivAleatorio = Math.floor(Math.random() * 100 + 1);
+
+
+
+/*for (var i = 0; i < buttonDelete.length; ++i) {
+    buttonDelete[i].addEventListener */
+
+  
+
+function excluirComentario() {
+    let pegarIdDiv = document.getElementById(idDivAleatorio);
+    if(pegarIdDiv) {
+        pegarIdDiv.classList.add('esconder');
+    }
+}
+
 
 send.addEventListener('click', criarComentario);
 
 function criarComentario() {
     let elementoPai = document.querySelector('section');
-    let criando = document.createElement('div');
-        criando.classList.add('coments');
-            criando.innerHTML = `<div class="likes">
-            <img class="icon-plus" src="./images/icon-plus.svg"><p>0</p><img class="icon-minus" src="./images/icon-minus.svg">
-          </div>
-          <div class="container-comentarios">
-            <div class="container-superior">
-                <div class="container-perfil">
-                  <div class="foto"><img src="./images/avatars/image-juliusomo.png"></div>
-                  <div class="nickname">amyrobson<span class="tag-you">you</span></div>
-                  <div class="date-publish">3 months ago</div>
-                </div>
-                <div class="container-reply-delete">
-                  <button class="reply"><img src="./images/icon-reply.svg">Reply</button>
-                  <button class="edit esconder"><img src="./images/icon-edit.svg">Edit</button>
-                  <button class="delete esconder"><img src="./images/icon-delete.svg">Delete</button>
-                </div>
+    const inputComent = document.querySelector('.escrever-comentario textarea').value;
+    if (inputComent != '') {
+        let criando = document.createElement('div');
+            criando.id = idDivAleatorio;
+            criando.classList.add('coments');
+                criando.innerHTML = `<div class="likes">
+                <img class="icon-plus" src="./images/icon-plus.svg"><p>0</p><img class="icon-minus" src="./images/icon-minus.svg">
             </div>
-          <div class="comentario">Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.</div>
-        </div>`;
-         elementoPai.appendChild(criando);
+            <div class="container-comentarios">
+                <div class="container-superior">
+                    <div class="container-perfil">
+                    <div class="foto"><img src="./images/avatars/image-juliusomo.png"></div>
+                    <div class="nickname">amyrobson<span class="tag-you">you</span></div>
+                    <div class="date-publish">few seconds</div>
+                    </div>
+                    <div class="container-reply-delete">
+                    <button class="reply esconder"><img src="./images/icon-reply.svg">Reply</button>
+                    <button class="edit"><img src="./images/icon-edit.svg">Edit</button>
+                    <button onclick = "excluirComentario()" class="delete"><img src="./images/icon-delete.svg">Delete</button>
+                    </div>
+                </div>
+            <div class="comentario">${inputComent}</div>
+            </div>`;
+            elementoPai.appendChild(criando);
+            let inputVazio = document.querySelector('.escrever-comentario textarea');
+            inputVazio.value = '';
+    }
 }
 
-for (var i = 0; i < reply.length; ++i) {
-    reply[i].addEventListener('click', replyComent);
-        function replyComent(){
-            console.log("tá prestando");
-        }
-}
 
-for (var i = 0; i < buttonDelete.length; ++i) {
-    buttonDelete[i].addEventListener('click', deleteComent);
-        function deleteComent(){
-            let removendoDiv = document.querySelector('.coments');
-            removendoDiv.classList.add ('esconder');
-        }
-}
 
-for (var i = 0; i < edit.length; ++i) {
-    edit[i].addEventListener('click', editComent);
-        function editComent(){
-            console.log("tá prestando");
-        }
-}
 
-for (var i = 0; i < likeAdicionar.length; ++i) {
-    likeAdicionar[i].addEventListener('click', adicionarLike);
-        function adicionarLike(){
-            console.log("tá prestando");
-        }
-}
-
-for (var i = 0; i < likeSubtrair.length; ++i) {
-    likeSubtrair[i].addEventListener('click', retirarLike);
-        function retirarLike(){
-            console.log("tá prestando");
-        }
-}
